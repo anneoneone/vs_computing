8c567fa457bf540dc9acd640b0c344cc75cd49b6
s72347 <s72347@beuth-hochschule.de>
2020-10-28 15:16:08 +0100

    A1 fertig (hohe gridzahl)

--- aufgabe3/tomography_solve.py
+++ aufgabe3/tomography_solve.py
@@ -80,7 +80,7 @@ def create_sinogram(nAngles, nSamples, angle_range=(0, np.pi)):
 # ---------------------------------------------
 # Main Programablauf:
 # ---------------------------------------------
-gridsizes = [ 16, 32 ] #, 16] #, 128, 256]
+gridsizes = [ 128, 256 ] #, 16] #, 128, 256]
 # plot mit unterfigures
 fig, ax = plt.subplots(nrows=2, ncols=len(gridsizes))
 # Für alle Gridsizes:
